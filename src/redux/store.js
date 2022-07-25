ee8f1e4f469ae65845633496e106f0a88b958944
Andrey Grebennikov <lostplanet08@mail.ru>
2022-07-25 21:32:11 +0400

    1. Fixed query params

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import sortReducer from "./slices/sortSlice";
 import cartReducer from "./slices/cartSlice";
 import pizzasReducer from "./slices/pizzasSlice";
 
 export const store = configureStore({
   reducer: {
-    sort: sortReducer,
     cart: cartReducer,
     pizzas: pizzasReducer,
   },
